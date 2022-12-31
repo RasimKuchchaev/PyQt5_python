e5df29d4e22a05b33e85776907ee2e69e077d919
RasimKuchchaev <rgkuchchaev@gmail.com>
2022-12-31 23:18:43 +0300

    QDateTime

---
+++ q_date_time_edit.py
@@ -0,0 +1,27 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+
+class DlgMain(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Main Windows')
+        self.resize(400, 200)
+
+        self.dteDate = QDateTimeEdit(QDate.currentDate(), self)
+        self.dteDate.move(50, 50)
+        self.dteDate.setCalendarPopup(True)
+
+        self.dteTime = QTimeEdit(QTime.currentTime(), self)
+        self.dteTime.move(50, 80)
+
+        self.dteDateTime = QDateTimeEdit(QDateTime().currentDateTime(), self)
+        self.dteDateTime.move(50, 110)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)        # create application
+    dlgMain = DlgMain()                 # create main GUI windows
+    dlgMain.show()                      # show GUI
+    sys.exit(app.exec_())               # execute the application
